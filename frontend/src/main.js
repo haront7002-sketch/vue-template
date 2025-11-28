fc9d3e7d8cd9a2c0b74345cf2d57b5769f028ed8
haront7002-sketch <haron.t.7002@gmail.com>
2025-11-28 12:11:39 +0200

    installed vuex and have to create a store folder with an index.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// installing bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// using vuex for data handling by importing my store from my main.js
+// import store from './store'
 
 const app = createApp(App)
 
 app.use(router)
+// using that store
+// app.use(store)
 
 app.mount('#app')
